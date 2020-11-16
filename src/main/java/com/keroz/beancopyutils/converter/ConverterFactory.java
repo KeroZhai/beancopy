0687af43e3117d44ab42fd377dd1d7373ad6cef2
KeroZhai <980580084@qq.com>
2020-11-16 16:30:57 +0800

    Check if the converter class is null

--- src/main/java/com/keroz/beancopyutils/converter/ConverterFactory.java
+++ src/main/java/com/keroz/beancopyutils/converter/ConverterFactory.java
@@ -9,21 +9,23 @@ public class ConverterFactory {
 
     public static Converter<?, ?> getConverter(Class<? extends Converter<?, ?>> converterClass) {
         Converter<?, ?> converter = null;
-        synchronized (CACHE) {
-            converter = CACHE.get(converterClass);
-        }
-        if (converter == null) {
+        if (converterClass != null) {
             synchronized (CACHE) {
                 converter = CACHE.get(converterClass);
-                if (converter == null) {
-                    try {
-                        converter = converterClass.newInstance();
-                    } catch (InstantiationException | IllegalAccessException e) {
-                        e.printStackTrace();
-                        throw new com.keroz.beancopyutils.exception.InstantiationException(
-                                "Failed to instantiate converter class: " + converterClass.getName(), e);
+            }
+            if (converter == null) {
+                synchronized (CACHE) {
+                    converter = CACHE.get(converterClass);
+                    if (converter == null) {
+                        try {
+                            converter = converterClass.newInstance();
+                        } catch (InstantiationException | IllegalAccessException e) {
+                            e.printStackTrace();
+                            throw new com.keroz.beancopyutils.exception.InstantiationException(
+                                    "Failed to instantiate converter class: " + converterClass.getName(), e);
+                        }
+                        CACHE.put(converterClass, converter);
                     }
-                    CACHE.put(converterClass, converter);
                 }
             }
         }
