46d0de0db49c6e00fe20daa85bd4c2500d4adf98
Kero <980580084@qq.com>
2022-06-23 16:54:21 +0800

    docs(CopyIgnore): update JavaDoc

--- src/main/java/com/keroz/beancopyutils/annotation/CopyIgnore.java
+++ src/main/java/com/keroz/beancopyutils/annotation/CopyIgnore.java
@@ -7,9 +7,9 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * Indicates {@code when} or {@code except} a certain condition is met that the
- * annotated field in a <strong>target</strong> class should be ignored (during
- * copying). A {@linkplain IgnorePolicy} can also be specified.
+ * Indicates whether the annotated field in a <strong>target</strong> class
+ * should be ignored (during copying). A {@link IgnorePolicy} can also be
+ * specified.
  * <p>
  * At least one attribute must be provided, otherwise this annotation will have
  * no effect. Futhermore, it can even be misleading, since a field annotated
@@ -18,31 +18,35 @@ import java.lang.annotation.Target;
  * The example below shows how to use this annotation.
  *
  * <pre>
- * {@code
+ * <code>
  * public class Foo {
- *     public static final String COPY_WITHOUT_ID = "copyWithoutID";
- *     public static final String COPY_WITH_NAME = "copyWithName";
+ *     public static interface ExcludingId {
+ *     }
  *
- *     {@literal @}CopyIgnore(when = COPY_WITHOUT_ID)
+ *     public static interface IncludingName {
+ *     }
+ *
+ *     {@literal @}CopyIgnore(defaultIgnored = false, exceptionGroups = ExcludingId.class)
  *     public int id;
- *     {@literal @}CopyIgnore(except = COPY_WITH_NAME)
+ *     {@literal @}CopyIgnore(exceptionGroups = IncludingName.class)
  *     public String name;
- * }}
+ * }
+ * </code>
  * </pre>
  *
- * The two {@code String} constants serve as conditions, which can be specified
+ * The two interfaces serve as groups, which can be specified
  * by providing an array when copying.
  *
  * <pre>
- * {@code
+ * <code>
  * Foo source = new Foo();
  * source.id = 1;
  * source.name = "foo";
  * Foo target0 = BeanCopyUtils.copy(source, Foo.class);
- * Foo target1 = BeanCopyUtils.copy(source, Foo.class, new String[] { Foo.COPY_WITHOUT_ID });
- * Foo target2 = BeanCopyUtils.copy(source, Foo.class, new String[] { Foo.COPY_WITH_NAME });
- * Foo target2 = BeanCopyUtils.copy(source, Foo.class, new String[] { Foo.COPY_WITHOUT_ID, Foo.COPY_WITH_NAME });
- * }
+ * Foo target1 = BeanCopyUtils.copy(source, Foo.class, new Class<?>[] { Foo.ExcludingId.class });
+ * Foo target2 = BeanCopyUtils.copy(source, Foo.class, new Class<?>[] { Foo.IncludingName.class });
+ * Foo target2 = BeanCopyUtils.copy(source, Foo.class, new Class<?>[] { Foo.ExcludingId.class, Foo.IncludingName.class });
+ * </code>
  * </pre>
  *
  * And the result comes out as below.
@@ -53,10 +57,6 @@ import java.lang.annotation.Target;
  * <li>target2: id = 1, name = "foo"</li>
  * <li>target3: id = 0, name = "foo"</li>
  * </ul>
- *
- * It is recommanded to declare {@code String} condtions as
- * {@code public static} so that they will be ignored. It's also a good practice
- * to make them unique, otherwise, the result may not meet expectations.
  *
  * @see IgnorePolicy
  */
